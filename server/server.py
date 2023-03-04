2d31fa3653136ccd6f8bcd3d3b4cecf0c3aacc3c
Jinqiu Deng <>
2023-03-04 15:32:24 +0800

    add param

--- server/server.py
+++ server/server.py
@@ -19,16 +19,18 @@ class MainHandler(tornado.web.RequestHandler):
         openai.organization = self.config.settings['openai_organization']
         openai.api_key = self.config.settings['openai_api_key']
 
+        question = self.get_argument('question')
+
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             temperature=0.2,
             max_tokens=20,
-            messages=[
-                {"role": "user", "content": "Redmi Note 10 Pro 5G 天玑1100旗舰芯 67W快充 120Hz旗舰变速金刚屏 幻青 8GB+128GB 智能手机 小米红米8GB+128GB 幻青 \n 小米Redmi Note10Pro 5G游戏智能手机 天玑1100旗舰芯 67W快充 机身颜色:幻青$$存储容量:6GB+128GB$$套餐类型:官方标配$ \n same sku?"}
-            ]
+            messages=[{ "role": "user", "content": question}]
         )
+
         print(completion)
         self.write(completion)
+
 
 config = Config(os.path.join(os.path.dirname(__file__), "openai_gpt_key.yaml"))
 
