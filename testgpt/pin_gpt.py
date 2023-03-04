1f3389a6a25f4f4e01041b586f12dd4da088b314
Jinqiu Deng <>
2023-03-04 11:39:21 +0800

    move gpt key to a config file

--- testgpt/pin_gpt.py
+++ testgpt/pin_gpt.py
@@ -1,15 +1,18 @@
+#coding=utf-8
 import os
 import openai
+import json
 
-openai.organization = "org-ZZjbb7CBD0aztOHPV7hrrhMR"
-openai.api_key = "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"
+with open('./gpt_key.json') as f:
+    data = json.load(f)
 
-print(1)
+openai.organization = data['organization']
+openai.api_key = data['api_key']
 
 completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  temperature=0.2,
-  messages=[
+    model="gpt-3.5-turbo",
+    temperature=0.2,
+    messages=[
         {"role": "user", "content": "Redmi Note 10 Pro 5G 天玑1100旗舰芯 67W快充 120Hz旗舰变速金刚屏 幻青 8GB+128GB 智能手机 小米红米8GB+128GB 幻青 \n 小米Redmi Note10Pro 5G游戏智能手机 天玑1100旗舰芯 67W快充 机身颜色:幻青$$存储容量:6GB+128GB$$套餐类型:官方标配$ \n 是否是相同的sku"}
     ]
 )
